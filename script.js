281ad2ee0b8577c82265d3c47ddcdeadbdbd35e9
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-08-06 17:21:22 +0530

    updated files

---
+++ script.js
@@ -0,0 +1,44 @@
+let bot = new RiveScript();
+
+const message_container = document.querySelector('.messages');
+const form = document.querySelector('form');
+const input_box = document.querySelector('input');
+
+const brains = [
+    'https://gist.githubusercontent.com/awesammcoder/91e0f6c527bfdc03b8815289ca4af150/raw/6410ce00b7e1ea0dbd28be03b6eaab64252a841d/brain.rive'
+
+
+    // 'https://gist.githubusercontent.com/flyingsonu122/7c7e86dee7f252acf59b561412042d24/raw/24e8123c6b327e018d33af2ee2fd410ead8219b4/brain.rive'
+    
+];
+
+bot.loadFile(brains).then(botReady).catch(botNotReady);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    selfReply(input_box.value);
+    input_box.value = '';
+});
+
+function botReply(message) {
+    message_container.innerHTML += `<div class="bot">${message}</div>`;
+    location.href = '#edge';
+}
+
+function selfReply(message) {
+    message_container.innerHTML += `<div class="self">${message}</div>`;
+    location.href = '#edge';
+
+    bot.reply("local-user", message).then(function (reply) {
+        botReply(reply);
+    });
+}
+
+function botReady() {
+    bot.sortReplies();
+    botReply('Hello');
+}
+
+function botNotReady(err) {
+    console.log("An error has occurred.", err);
+}
